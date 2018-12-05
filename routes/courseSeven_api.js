c02148c2a7fe3dcf9b29387e56d99f39be4784cd
Sagar Shikhare <sagar.s@peopleinteractive.in>
2018-12-05 19:37:06 +0530

    Final commit

--- routes/courseSeven_api.js
+++ routes/courseSeven_api.js
@@ -1,8 +1,8 @@
 var mysql = require('mysql');
 
 var con = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
+  host: '127.0.0.1',
+  user: 'workbench',
   password: 'root',
   database: 'vwcs',
 });
