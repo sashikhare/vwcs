316fd7856ab5239695fc31a5801d8b1a1c3d1487
Sagar Shikhare <sagar.s@peopleinteractive.in>
2018-12-06 19:40:12 +0530

    Office time change

--- src/components/contatPage/ContactPageBodyDetails_Second.js
+++ src/components/contatPage/ContactPageBodyDetails_Second.js
@@ -88,7 +88,7 @@ class ContactPageBodyDetails_Second extends React.Component {
                 <div class="contact-details">
                   <h5>+91 (961) 9123 321</h5>
                   <h5>+91 (900) 4935 111</h5>
-                  <p>Mon to Sat 9am to 6 pm</p>
+                  <p>Mon to Sat 9am to 5 pm</p>
                 </div>
               </div>
               <div class="single-contact-address d-flex flex-row">
