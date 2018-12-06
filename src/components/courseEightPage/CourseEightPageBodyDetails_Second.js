fad15a2c8ad9a290a4176d22d010d664f2a955de
Sagar Shikhare <sagar.s@peopleinteractive.in>
2018-12-06 18:32:12 +0530

    Course Eight change

--- src/components/courseEightPage/CourseEightPageBodyDetails_Second.js
+++ src/components/courseEightPage/CourseEightPageBodyDetails_Second.js
@@ -35,18 +35,9 @@ class CourseEightPageBodyDetails_Second extends React.Component {
                     <u>Course Objective</u>
                   </h4>
                   <p class="header-gap" align="justify">
-                    The objective is to lay a solid foundation in well control concepts and competency for entry-level rig operation
-                    positions as well as those not directly associated with rig operation, such as office personnel. This course is strongly
-                    recommended for first time well control candidates and prior to taking IWCF Well Control Course in Level 3 and then
-                    Level 4. The objective of this training is to increase the candidates knowledge and understanding of well control
-                    theory, practice and equipment. This will help prepare the candidates to deal with most well control situations.
-                  </p>
-                  <p align="justify">
-                    U-Tube concept, gas laws, basic math skills, pressure basics, kick detection, kill sheet, drilling fluids, BOP
-                    equipment, shut-in procedures, and basic methods of well control etc. will be all covered. At the end of this course,
-                    delegates should be able to demonstrate familiarity with an understanding of the equipment, principles and procedures
-                    associated with rotary drilling well control in preparation for IWCF Well Control testing at Level 2. The successful
-                    candidate will be issued with a certificate valid for five years.
+                    IADC Accredited HSE Rig Pass program ensures that personnel have met basic requirements defined by safety and training
+                    professionals in the drilling industry, irrespective of the rig’s location. IADC Rig Pass is designed to meet the
+                    requirement of to identify core elements of safety orientation programs for new rig employee.
                   </p>
                 </div>
                 <div>
@@ -54,35 +45,9 @@ class CourseEightPageBodyDetails_Second extends React.Component {
                     <u>Course Overview</u>
                   </h4>
                   <p class="header-gap" align="justify">
-                    The course duration is five days. Course starts on Tuesday at 08:30 hrs. & finishes on Saturday at 14:00 hrs. For first
-                    three days extensive class room teaching is followed by practical sessions on simulator. Assignment containing questions
-                    pertaining to days curriculum are given and discussion on assignment is taken up in the next morning.
+                    ADC HSC Rig Pass is a one-day course, conducted every Monday. Class starts sharp at 9:00 am till 5:00pm. A full day
+                    lecture will be conducted and by end of the day your performance will be reviewed and certificate will be issued.
                   </p>
-                  <p align="justify">
-                    On fourth day a mock test is conducted which is an in- house test based on IWCF pattern. This is to access the
-                    performance of the participants and prepare them for the final test. Individual attention is given to the participants
-                    based on the results of the Mock test. Simulator Practical Assessment. On fifty day, there will be written examination
-                    and certification.
-                  </p>
-                  <p align="justify">
-                    DAY 1 : Well Control Event & Incident, Well Control Training Assessment, Risk Management, Introduction to Well Control
-                    and Pressure, Barrier Concept, Kick and Concept of Bottomhole Pressure, Kick warning Sign and Kick Indicator
-                    (Detection), Top Hole Drilling, Kill Sheet preparation, Simulator hand on practice.
-                  </p>
-                  <p align="justify">
-                    DAY 2: Assignment Discussion, Concept of Circulating System, Concept of Fracture Pressure and MAASP, Influx
-                    Characteristic and its behavior, Shut in Procedure, Gas Migration, Well Control Method, Kill Sheet preparation,
-                    Simulator hand on practice.
-                  </p>
-                  <p align="justify">
-                    DAY 3: Assignment Discussion, Volumetric Method, Stripping, Well Control During Casing and Cementing Operation, Well
-                    control Management, Contigency Planning, Well Control Equipment, Ram Type Preventer, Associated Well Control Equipment,
-                    BOP and Equipment Test, Inflow Test, Simulator hand on practice.
-                  </p>
-                  <p align="justify">
-                    DAY 4: Assignment Discussion, BOP Control System, Subsea BOP Control System, MOCK Test, Simulation Practical Assessment.
-                  </p>
-                  <p align="justify">DAY 5: Examination and Certification.</p>
                 </div>
                 <div>
                   <h4 align="left">
